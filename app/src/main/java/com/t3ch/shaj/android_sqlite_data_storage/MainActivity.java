d59ba0ec54daa38e639c08527c4507bd1ae71a69
SHAJ <shakilahmedshaj@gmail.com>
2018-10-21 18:49:58 +0600

    Initial commit

---
+++ app/src/main/java/com/t3ch/shaj/android_sqlite_data_storage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.t3ch.shaj.android_sqlite_data_storage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
